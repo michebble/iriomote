72eb0cc78813df5946e15f3fab762dfd2e66a774
michebble <32097294+michebble@users.noreply.github.com>
2018-11-27 13:27:09 +1100

    Test routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :employees, only: [:index, :new, :show]
+
+
+
+  resources :teams do
+    resources :periods, shallow: true
+  end
+
+  resources :periods do
+    resources :objectives, shallow: true
+  end
+
+  resources :objectives do
+    resources :key_results, shallow: true
+  end
+
+  resources :key_results
 end
